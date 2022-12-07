f33b56add30e2dbb11ed9e839d12261f120bd3a7
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-12-07 12:01:56 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,19 @@
 
+#include <iostream>
+#include "lib.h"
+
+using namespace std;
+
+bool lettera(char &a){
+    if (a >=65 && a <=90 )
+    {
+        a+= 32;
+        return true;
+    }
+    else if(a >=97 && a<=122 ){
+        a-=32;
+        return true;
+    }
+    else
+        return false;
+}
